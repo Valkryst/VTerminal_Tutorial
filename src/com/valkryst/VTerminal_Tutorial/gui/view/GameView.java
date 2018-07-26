7ed4ea3657d6af8486d96a9e8b83997cb3e20c76
Valkryst <valkryst@valkryst.com>
2018-07-26 18:25:03 -0300

    Adds the GameView class.

---
+++ src/com/valkryst/VTerminal_Tutorial/gui/view/GameView.java
@@ -0,0 +1,15 @@
+package com.valkryst.VTerminal_Tutorial.gui.view;
+
+import com.valkryst.VTerminal.Screen;
+
+public class GameView extends View {
+    /**
+     * Constructs a new GameView.
+     *
+     * @param screen
+     *          The screen on which the view is displayed.
+     */
+    public GameView(final Screen screen) {
+        super(screen);
+    }
+}
