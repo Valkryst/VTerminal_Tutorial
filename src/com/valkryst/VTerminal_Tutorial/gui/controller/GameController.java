91f727aa8c4e746750ade740325b20f211c6f393
Valkryst <valkryst@valkryst.com>
2018-07-26 18:59:14 -0300

    Sets the initializeEventHandlers function as private and sets the constructor to run it right away.
    Adds the timer instance variable as a game loop.

--- src/com/valkryst/VTerminal_Tutorial/gui/controller/GameController.java
+++ src/com/valkryst/VTerminal_Tutorial/gui/controller/GameController.java
@@ -4,12 +4,17 @@ import com.valkryst.VTerminal.Screen;
 import com.valkryst.VTerminal_Tutorial.entity.Player;
 import com.valkryst.VTerminal_Tutorial.gui.model.GameModel;
 import com.valkryst.VTerminal_Tutorial.gui.view.GameView;
+import lombok.Getter;
 import lombok.NonNull;
 
+import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class GameController extends Controller<GameView, GameModel> {
+    /** The timer which acts as an extremely basic game loop. */
+    @Getter private final Timer timer;
+
     /**
      * Constructs a new GameController.
      *
@@ -21,6 +26,11 @@ public class GameController extends Controller<GameView, GameModel> {
      */
     public GameController(final @NonNull Screen screen) {
         super(new GameView(screen), new GameModel(screen));
+        initializeEventHandlers(screen);
+
+        timer = new Timer(200, e -> {
+            screen.draw();
+        });
     }
 
     /**
@@ -29,7 +39,7 @@ public class GameController extends Controller<GameView, GameModel> {
      * @param screen
      *          The screen on which the view is displayed.
      */
-    public void initializeEventHandlers(final Screen screen) {
+    private void initializeEventHandlers(final Screen screen) {
         final Player player = super.model.getPlayer();
 
         final KeyListener keyListener = new KeyListener() {
