bf8ac4ff12cda7d6cda7f60c3e1f2c2024d17d18
Valkryst <valkryst@valkryst.com>
2018-07-26 17:56:43 -0300

    Adds the MainMenuController class.

---
+++ src/com/valkryst/VTerminal_Tutorial/gui/controller/MainMenuController.java
@@ -0,0 +1,40 @@
+package com.valkryst.VTerminal_Tutorial.gui.controller;
+
+import com.valkryst.VTerminal.Screen;
+import com.valkryst.VTerminal_Tutorial.gui.model.MainMenuModel;
+import com.valkryst.VTerminal_Tutorial.gui.view.MainMenuView;
+import lombok.NonNull;
+
+public class MainMenuController extends Controller<MainMenuView, MainMenuModel> {
+    /**
+     * Constructs a new MainMenuController.
+     *
+     * @param screen
+     *          The screen on which the view is displayed.
+     *
+     * @throws NullPointerException
+     *          If the screen is null.
+     */
+    public MainMenuController(final @NonNull Screen screen) {
+        super(new MainMenuView(screen), new MainMenuModel());
+        initializeEventHandlers(screen);
+        screen.addComponent(super.getView());
+        screen.draw();
+    }
+
+    /**
+     * Creates any event handlers required by the view.
+     *
+     * @param screen
+     *          The screen on which the view is displayed.
+     */
+    public void initializeEventHandlers(final Screen screen) {
+        super.view.getButton_new().setOnClickFunction(() -> {
+            Controller.swapViews(screen, new GameController(screen));
+        });
+
+        super.view.getButton_exit().setOnClickFunction(() -> {
+            System.exit(0);
+        });
+    }
+}
