df58081b63afc151afd5ed3433e20c68eb2bbb74
Valkryst <valkryst@valkryst.com>
2018-08-07 19:18:49 -0300

    Reverts previous commit.

--- src/com/valkryst/VTerminal_Tutorial/gui/view/GameView.java
+++ src/com/valkryst/VTerminal_Tutorial/gui/view/GameView.java
@@ -1,6 +1,9 @@
 package com.valkryst.VTerminal_Tutorial.gui.view;
 
 import com.valkryst.VTerminal.Screen;
+import com.valkryst.VTerminal_Tutorial.Map;
+import com.valkryst.VTerminal_Tutorial.entity.Player;
+import com.valkryst.VTerminal_Tutorial.gui.model.GameModel;
 
 public class GameView extends View {
     /**
@@ -11,5 +14,21 @@ public class GameView extends View {
      */
     public GameView(final Screen screen) {
         super(screen);
+    }
+
+    /**
+     * Adds any components, defined in the model, to the view.
+     *
+     * @param model
+     *          The model.
+     */
+    public void addModelComponents(final GameModel model) {
+        final Map map = model.getMap();
+        final Player player = model.getPlayer();
+
+        this.addComponent(map);
+
+        this.addComponent(player);
+        player.getLineOfSight().showLOSOnMap(map);
     }
 }
