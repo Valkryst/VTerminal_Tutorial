3e04c390df0feba8088bb9b354dd390e08f4f1d7
Valkryst <valkryst@valkryst.com>
2018-08-10 13:56:21 -0300

    Adds the DodgeAction class.

---
+++ src/com/valkryst/VTerminal_Tutorial/action/DodgeAction.java
@@ -0,0 +1,11 @@
+package com.valkryst.VTerminal_Tutorial.action;
+
+import com.valkryst.VTerminal_Tutorial.entity.Entity;
+import com.valkryst.VTerminal_Tutorial.gui.controller.GameController;
+
+public class DodgeAction extends Action {
+    @Override
+    public void perform(final GameController controller, final Entity entity) {
+        super.perform(controller, entity);
+    }
+}
