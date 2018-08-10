63a53654c9f6976dbf51c750a477c99d4a4c227e
Valkryst <valkryst@valkryst.com>
2018-08-10 15:07:45 -0300

    Rewrites the perform function to allow for attacking.

--- src/com/valkryst/VTerminal_Tutorial/action/MoveAction.java
+++ src/com/valkryst/VTerminal_Tutorial/action/MoveAction.java
@@ -7,6 +7,7 @@ import com.valkryst.VTerminal_Tutorial.entity.Player;
 import com.valkryst.VTerminal_Tutorial.gui.controller.GameController;
 
 import java.awt.*;
+import java.util.List;
 
 public class MoveAction extends Action {
     /** The original position of the entity being moved. */
@@ -39,24 +40,46 @@ public class MoveAction extends Action {
     }
 
     @Override
-    public void perform(final GameController controller, final Entity entity) {
+    public void perform(final GameController controller, final Entity self) {
         final Map map = controller.getModel().getMap();
 
-        if (map == null || entity == null) {
+        if (map == null || self == null) {
             return;
         }
 
-        if (map.isPositionFree(newPosition)) {
-            super.perform(controller, entity);
-            entity.getTiles().setPosition(newPosition);
+        // Attack any enemies at new location:
+        final List<Entity> entities = map.getEntities();
 
-            if (entity instanceof Player) {
-                final LineOfSight los = entity.getLineOfSight();
+        if (entities.size() >= 1) {
+            for (final Entity target : entities) {
+                // If the Entity being moved is the player, then we attack non-player entities.
+                // Else if the Entity being moved isn't the player, then we attack player entities.
+                if (self instanceof Player) {
+                    if (target instanceof Player == false) {
+                        new AttackAction(target).perform(controller, self);
+                        return;
+                    }
+                } else {
+                    if (target instanceof Player) {
+                        new AttackAction(target).perform(controller, self);
+                        return;
+                    }
+                }
+            }
+        }
+
+        // Move to the new location:
+        if (map.isPositionFree(newPosition)) {
+            super.perform(controller, self);
+            self.getTiles().setPosition(newPosition);
+
+            if (self instanceof Player) {
+                final LineOfSight los = self.getLineOfSight();
                 los.hideLOSOnMap(map);
                 los.move(dx, dy);
                 los.showLOSOnMap(map);
             } else {
-                entity.getLineOfSight().move(dx, dy);
+                self.getLineOfSight().move(dx, dy);
             }
         }
     }
