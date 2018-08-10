5d7c3c66cb369e4f45442ba1a510ace2c844aec2
Valkryst <valkryst@valkryst.com>
2018-08-10 14:18:09 -0300

    Adds the DeathAction class.

---
+++ src/com/valkryst/VTerminal_Tutorial/action/DeathAction.java
@@ -0,0 +1,24 @@
+package com.valkryst.VTerminal_Tutorial.action;
+
+import com.valkryst.VTerminal_Tutorial.Map;
+import com.valkryst.VTerminal_Tutorial.Message;
+import com.valkryst.VTerminal_Tutorial.entity.Entity;
+import com.valkryst.VTerminal_Tutorial.gui.controller.GameController;
+
+public class DeathAction extends Action {
+    @Override
+    public void perform(final GameController controller, final Entity self) {
+        super.perform(controller, self);
+
+        if (controller == null || self == null) {
+            return;
+        }
+
+        final Map map = controller.getModel().getMap();
+
+        if (map != null) {
+            map.getEntities().remove(self);
+            controller.displayMessage(new Message().appendEntityName(self).append(" has died."));
+        }
+    }
+}
