23b8c3594357981f2dea988db91482efa5622918
Valkryst <valkryst@valkryst.com>
2018-08-15 11:07:02 -0300

    Adds the rollDamage and getArmor functions.

--- src/com/valkryst/VTerminal_Tutorial/item/Equipment.java
+++ src/com/valkryst/VTerminal_Tutorial/item/Equipment.java
@@ -1,6 +1,8 @@
 package com.valkryst.VTerminal_Tutorial.item;
 
+import com.valkryst.VDice.DiceRoller;
 import com.valkryst.VTerminal_Tutorial.Sprite;
+import com.valkryst.VTerminal_Tutorial.statistic.BoundStat;
 import com.valkryst.VTerminal_Tutorial.statistic.Stat;
 import lombok.Getter;
 
@@ -31,5 +33,48 @@ public class Equipment extends Item {
     public Equipment(final Sprite sprite, final String name, final String description, final HashMap<String, Stat> stats, final EquipmentSlot slot) {
         super(sprite, name, description, stats);
         this.slot = (slot == null ? EquipmentSlot.UNKNOWN : slot);
+    }
+
+    /**
+     * Uses the damage stat to perform a damage roll. The result is a value between the min and max damage
+     * values of the stat.
+     *
+     * If this piece of equipment doesn't have a damage stat, then 0 is returned.
+     *
+     * @return
+     *          The rolled damage value.
+     */
+    public int rollDamage() {
+        final BoundStat damageStat = (BoundStat) super.getStat("Damage");
+
+        if (damageStat == null) {
+            return 0;
+        }
+
+        final int minDamage = damageStat.getMinValue();
+        final int maxDamage = damageStat.getMaxValue();
+
+        final DiceRoller diceRoller = new DiceRoller();
+        diceRoller.addDice(1, maxDamage - minDamage);
+
+        return diceRoller.roll() + minDamage;
+    }
+
+    /**
+     * Retrieves the armor value from the armor stat.
+     *
+     * If this piece of equipment doesn't have an armor stat, then 0 is returned.
+     *
+     * @return
+     *          The armor value of the armor stat.
+     */
+    public int getArmor() {
+        final Stat armor = super.getStat("Armor");
+
+        if (armor == null) {
+            return 0;
+        }
+
+        return armor.getValue();
     }
 }
