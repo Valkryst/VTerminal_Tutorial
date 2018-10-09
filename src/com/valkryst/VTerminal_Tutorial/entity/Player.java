4b56f0c266016b09c45a2e29b8e3002f741662b7
Valkryst <valkryst@valkryst.com>
2018-10-09 10:33:00 -0300

    Fixes wording of constructor's JavaDoc. Adds missing information to constructor's JavaDoc.

--- src/com/valkryst/VTerminal_Tutorial/entity/Player.java
+++ src/com/valkryst/VTerminal_Tutorial/entity/Player.java
@@ -9,12 +9,14 @@ public class Player extends Entity {
      * Constructs a new Player.
      *
      * @param position
-     *          The position of the entity within a map.
+     *          The position of the player within a map.
      *
      *          Defaults to (0, 0) if the position is null or if either part of the coordinate is negative.
      *
      * @param name
      *          The name.
+     *
+     *          Defaults to NoNameSet if the name is null or empty.
      */
     public Player(final Point position, final String name) {
         super(Sprite.PLAYER, position, name);
