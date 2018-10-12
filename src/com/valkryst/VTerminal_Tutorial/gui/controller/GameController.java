c6bdb17b149e2656465b82fa9904d3e16ba09e4e
Valkryst <valkryst@valkryst.com>
2018-10-12 14:31:36 -0300

    Adds the addEntityToMap and removeEntityFromMap functions. Adds support to allow the user to enter the InventoryView.

--- src/com/valkryst/VTerminal_Tutorial/gui/controller/GameController.java
+++ src/com/valkryst/VTerminal_Tutorial/gui/controller/GameController.java
@@ -3,21 +3,25 @@ package com.valkryst.VTerminal_Tutorial.gui.controller;
 import com.valkryst.VTerminal.Screen;
 import com.valkryst.VTerminal_Tutorial.Map;
 import com.valkryst.VTerminal_Tutorial.Message;
+import com.valkryst.VTerminal_Tutorial.entity.Container;
 import com.valkryst.VTerminal_Tutorial.entity.Entity;
 import com.valkryst.VTerminal_Tutorial.entity.Player;
 import com.valkryst.VTerminal_Tutorial.gui.model.GameModel;
-import com.valkryst.VTerminal_Tutorial.gui.model.InventoryModel;
 import com.valkryst.VTerminal_Tutorial.gui.view.GameView;
 import com.valkryst.VTerminal_Tutorial.item.Inventory;
 import lombok.Getter;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class GameController extends Controller<GameView, GameModel> {
     /** The timer which runs the game loop. */
     @Getter private final Timer timer;
+
+    /** The controller for the inventory view. */
+    private final InventoryController inventoryController;
 
     /**
      * Constructs a new GameController.
@@ -30,6 +34,9 @@ public class GameController extends Controller<GameView, GameModel> {
         initializeEventHandlers(screen);
 
         super.view.addModelComponents(model);
+
+        // Create inventory controller
+        inventoryController = new InventoryController(screen, this);
 
         // Create and start the game-loop timer.
         final Map map = model.getMap();
@@ -55,7 +62,6 @@ public class GameController extends Controller<GameView, GameModel> {
      */
     private void initializeEventHandlers(final Screen screen) {
         final Player player = super.model.getPlayer();
-        final GameController gameController = this;
 
         final KeyListener keyListener = new KeyListener() {
             @Override
@@ -97,13 +103,29 @@ public class GameController extends Controller<GameView, GameModel> {
                         // Remove this view from the screen.
                         GameController.super.removeFromScreen(screen);
 
-                        // Add the new view to the screen.
-                        final Inventory playerInventory = GameController.super.model.getPlayer().getInventory();
-                        // todo Deal with loot inventory
+                        // If there's a container below the player, open it for looting.
+                        Inventory lootInventory = null;
 
-                        final InventoryModel model = new InventoryModel(playerInventory);
-                        final InventoryController controller = new InventoryController(screen, gameController, model);
-                        controller.addToScreen(screen);
+                        for (final Entity entity : model.getMap().getEntities()) {
+                            if (entity instanceof Container == false) {
+                                continue;
+                            }
+
+                            final Point containerPosition = entity.getPosition();
+                            final Point playerPosition = model.getPlayer().getPosition();
+
+                            if (containerPosition.equals(playerPosition)) {
+                                lootInventory = entity.getInventory();
+                                removeEntityFromMap(entity);
+                                break;
+                            }
+                        }
+
+                        // Add the new view to the screen.
+                        inventoryController.getModel().setPlayerInventory(model.getPlayer().getInventory());
+                        inventoryController.getModel().setLootInventory(lootInventory);
+
+                        inventoryController.addToScreen(screen);
                         break;
                     }
                 }
@@ -111,6 +133,41 @@ public class GameController extends Controller<GameView, GameModel> {
         };
 
         super.getModel().getEventListeners().add(keyListener);
+    }
+
+    /**
+     * Adds an entity to the map.
+     *
+     * @param entity
+     *          The entity to add.
+     */
+    public void addEntityToMap(final Entity entity) {
+        if (entity == null) {
+            return;
+        }
+
+        final Map map = model.getMap();
+
+        if (map.getEntities().contains(entity) == false) {
+            map.getEntities().add(entity);
+            view.addComponent(entity);
+        }
+    }
+
+    /**
+     * Removes an entity from the map.
+     *
+     * @param entity
+     *          The entity to remove.
+     */
+    public void removeEntityFromMap(final Entity entity) {
+        if (entity == null) {
+            return;
+        }
+
+        final Map map = model.getMap();
+        map.getEntities().remove(entity);
+        view.removeComponent(entity);
     }
 
     /**
