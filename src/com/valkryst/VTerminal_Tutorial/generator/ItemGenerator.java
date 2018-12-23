c2f735c15f074a860f7266b30b7ab660e3a76a71
Valkryst <valkryst@valkryst.com>
2018-12-23 10:39:02 -0400

    Adds the ItemGenerator class.

---
+++ src/com/valkryst/VTerminal_Tutorial/generator/ItemGenerator.java
@@ -0,0 +1,13 @@
+package com.valkryst.VTerminal_Tutorial.generator;
+
+import com.valkryst.VTerminal_Tutorial.item.Item;
+
+public abstract class ItemGenerator {
+    /**
+     * Generates an item.
+     *
+     * @return
+     *          The generated item.
+     */
+    abstract public Item generate();
+}
